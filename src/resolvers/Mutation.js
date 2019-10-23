0fe0b3ee99e480707546180cccfd229c8071f193
Alex Buenrostro <alex101211b@gmail.com>
2019-10-22 23:45:25 -0500

    Possibles mutation and querys created

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,28 +1,57 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
-    async createUser(parent, args, ctx, info) {
+    async signUp(parent, args, ctx, info) {
         //lowercase their email
-        //args.email = args.email.toLowerCase();
+        args.email = args.email.toLowerCase();
         //hash their password
-        //const password = await bcrypt.hash(args.password, 10);
+        const password = await bcrypt.hash(args.password, 10);
 
         const user = await ctx.db.mutation.createUser({
             data: {
                 ...args,
-                //password,
-                //roles: { set: ['USER'] },
+                password,
             }
         }, info);
         // Create JWT token for them
-        //const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
         // We set the jwt as a cookie on the response
-        //ctx.response.cookie('token', token, {
-            //httpOnly: true,
-            //maxAge: 1000 * 60 * 60 * 24 * 365, // one year cookie
-        //});
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // one year cookie
+        });
         console.log(user);
         // return the user
         return user;
-    }
+    },
+    async createTicket(parent, args, ctx, info) {
+        const ticket = await ctx.db.mutation.createTicket({
+            data: {
+                ...args
+            }
+        }, info);
+
+        console.log(ticket);
+        return ticket;
+
+    },
+    async commentTicket(parent, args, ctx, info) {
+        const commentedTicket = await ctx.db.mutation.updateTicket({
+                ...args
+        }, info);
+
+        console.log('commentedTicked: ', commentedTicket);
+        return commentedTicket;
+    },
+    async assignDeveloper(parent, args, ctx, info) {
+        const assignedDeveloper = await ctx.db.mutation.updateTicket({
+                ...args
+        }, info);
+
+        console.log('assignedDeveloper: ', assignedDeveloper);
+        return assignedDeveloper;
+    },
 };
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,13 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
     users: forwardTo('db'),
+    async signIn(parent, args, ctx, info) {
+        console.log('args: ', args);
+        const user = await ctx.db.query.user({ ...args });
+        console.log('user: ', user);
+        return user;
+    },
+    tickets: forwardTo('db')
 };
 
 module.exports = Query;
